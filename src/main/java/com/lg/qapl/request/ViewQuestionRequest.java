ef48de3ff7e06ac6bc603c1026536205d643e960
陈周京 <11075633+chenzhoujing@user.noreply.gitee.com>
2023-12-01 02:45:21 +0100

    question

--- src/main/java/com/lg/qapl/request/ViewQuestionRequest.java
+++ src/main/java/com/lg/qapl/request/ViewQuestionRequest.java
@@ -4,6 +4,8 @@ import lombok.Data;
 
 @Data
 public class ViewQuestionRequest {
+    private boolean notAnswered;
+    private boolean thisMonth;
     private Integer size;
     private Integer current;
 }
