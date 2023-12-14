e57a5f446f16fcc6a970d2877513bd461c96741e
陈周京 <11075633+chenzhoujing@user.noreply.gitee.com>
2023-12-14 22:23:59 +0100

    开放端口

--- src/main/java/com/lg/qapl/config/WebConfig.java
+++ src/main/java/com/lg/qapl/config/WebConfig.java
@@ -10,9 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost")
+                .allowedOriginPatterns("*") // Use patterns for allowed origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(true); // Allow credentials
     }
 }
